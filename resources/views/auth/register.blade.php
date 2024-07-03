164c224332de6c8bdab56464dfca5de1511c6b99
Vitor Tester <vitor@teste.com>
2024-07-03 20:34:14 -0300

    adding preencher option to auto write the register fields

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,8 +40,14 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" 
+            href="{{ route('login') }}">
                 {{ __('Already registered?') }}
+            </a>
+
+            <a class="px-2 underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" 
+            href="#" id="preencher">
+                Preencher
             </a>
 
             <x-primary-button class="ms-4">
@@ -49,4 +55,15 @@
             </x-primary-button>
         </div>
     </form>
+
+    <script>
+        document.querySelector('#preencher').addEventListener('click', function(){
+            document.querySelector('#name').value =  'Jetete'
+            document.querySelector('#email').value =  'souteste@teste.com'
+            document.querySelector('#password').value =  'teste123'
+            document.querySelector('#password_confirmation').value =  'teste123'
+
+
+        })
+    </script>
 </x-guest-layout>
