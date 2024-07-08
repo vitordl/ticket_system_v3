fc8b9808ebf73ea3025b0ec25e4440dc20a03536
Vitor Tester <vitor@teste.com>
2024-07-08 11:04:50 -0300

    att

--- resources/views/livewire/reply-ticket-livewire.blade.php
+++ resources/views/livewire/reply-ticket-livewire.blade.php
@@ -18,14 +18,14 @@
     <div class="mt-8 text-sm">
         @if($replies)
             @foreach ($replies as $r)
-              <div class="border border-4  bg-gray-50 p-4 rounded-lg mt-4 ">
+              <div class="border border-4  bg-gray-50 p-2 rounded-lg mt-4 ">
                 @if($r->user->isAdmin)
                     <p class="text-red-600 text-xs bg-red-200 inline-flex px-1 rounded">{{$r->user->name}}</p>
                 @else  
                     <p class="text-blue-600 text-xs bg-gray-200 inline-flex px-1 rounded">{{$r->user->name}}</p>
                 @endif
-                <div>{{$r->reply}}</div>
-                <div class="text-xs text-end">{{$r->created_at->format('d/m/y H:i')}}</div>
+                <div class="px-1">{{$r->reply}}</div>
+                <div class="text-xs text-end text-gray-600">{{$r->created_at->format('d/m/y H:i')}}</div>
               </div>  
             @endforeach
         @else
